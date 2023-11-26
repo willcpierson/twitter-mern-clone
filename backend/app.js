22f4c7363dd43e50f05e422b6ab12878512badcc
Will Pierson <willcpierson@gmail.com>
2023-11-26 19:36:49 +0000

    add user validation

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
+require('./models/User')
+
 const usersRouter = require('./routes/api/users'); // update the import file path
 const tweetsRouter = require('./routes/api/tweets');
 const csrfRouter = require('./routes/api/csrf')
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,10 +1,53 @@
+const mongoose = require('mongoose');
+
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = mongoose.model('User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
     message: "GET /api/users"
+  });
+});
+
+// POST /api/users/register
+router.post('/register', async (req, res, next) => {
+    // Check to make sure no one has already registered with the proposed email or
+  // username.
+  const user = await User.findOne({
+    $or: [{ email: req.body.email }, { username: req.body.username }]
+  });
+
+  if (user) {
+    // Throw a 400 error if the email address and/or username already exists
+    const err = new Error("Validation Error");
+    err.statusCode = 400;
+    const errors = {};
+    if (user.email === req.body.email) {
+      errors.email = "A user has already registered with this email";
+    }
+    if (user.username === req.body.username) {
+      errors.username = "A user has already registered with this username";
+    }
+    err.errors = errors;
+    return next(err);
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) throw err;
+    bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
+      if (err) throw err;
+      try {
+        newUser.hashedPassword = hashedPassword;
+        const user = await newUser.save();
+        return res.json({ user });
+      }
+      catch(err) {
+        next(err);
+      }
+    })
   });
 });
 
